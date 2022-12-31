c2880d03f82bb9e220a2499c8bfd80c91f1fd1ee
CyberSpaceBoy <80852240+CyberSpaceBoy@users.noreply.github.com>
2022-12-31 10:34:34 -0800

    Update Search.js

--- Search.js
+++ Search.js
@@ -1,7 +1,5 @@
 document.getElementById('submit').onclick = () => {
     let user = document.getElementById('user').value;
-    console.log(user);
-   
   chrome.tabs.create({
     url: 'https://www.roblox.com/User.aspx?Username='+user,
     active: true
